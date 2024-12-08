1cc75cfd16333a71f3bea09987acca973cc83499
kai <137367860+Itzsoham@users.noreply.github.com>
2024-12-08 20:28:11 +0530

    finished signin signup and mobile navagation

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
 
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", username: "", name: "" }}
+      onSubmit={(values) => Promise.resolve({ success: true, values })}
+    />
+  );
 };
 
 export default SignUp;
