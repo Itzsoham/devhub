a43cba24fe322dbd5d1a0b80aa66352cea22eed7
kai <137367860+Itzsoham@users.noreply.github.com>
2024-12-10 22:38:07 +0530

    add question form and markdown editor

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -37,7 +37,7 @@ const questions = [
     author: {
       _id: "1",
       name: "John Doe",
-      image:
+      image:  
         "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
     },
     upvotes: 10,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import tailwindcssTypography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
@@ -59,7 +60,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, tailwindcssTypography],
 };
 
 export default config;
